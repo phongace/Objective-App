14e53df69855ac055b1a18c3a959009734384b06
Phong Nguyen Hoang <phongnh@timebird.org>
2021-02-24 10:50:37 +0700

    common base input

--- lib/screens/login/login-screen.dart
+++ lib/screens/login/login-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:objective/widgets/background.dart';
+import 'package:objective/widgets/base-input.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,6 +8,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var _username = TextEditingController();
+  var _password = TextEditingController();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,28 +20,31 @@ class _LoginScreenState extends State<LoginScreen> {
           Background(),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Hello',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
+            child: Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Login Now!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(height: 10),
+                  Text(
+                    'Login Now!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
+                  _buildBody(),
+                ],
+              ),
             ),
           ),
         ],
@@ -46,7 +54,25 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget _buildBody() {
     return Form(
-      child: Column(),
+      key: _formKey,
+      child: Column(
+        children: [
+          BaseTextInput(
+            hint: 'Username',
+            textCtrl: _username,
+            icon: Icons.person,
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.emailAddress,
+            validator: (val) => val.isEmpty ? "Email không được để trống!" : null,
+          ),
+          const SizedBox(height: 10),
+          BaseTextInput(
+            hint: 'Password',
+            textCtrl: _password,
+            validator: (val) => val.isEmpty ? "Password không được để trống!" : null,
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/base-input.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:objective/styles/component.dart';
+
+class BaseTextInput extends StatelessWidget {
+  final TextEditingController textCtrl;
+  final Function(String) onChangeText;
+  final String hint;
+  final FormFieldValidator<String> validator;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
+  final bool obscureText;
+  final IconData icon;
+
+  const BaseTextInput({
+    Key key,
+    this.textCtrl,
+    this.onChangeText,
+    this.hint,
+    this.validator,
+    this.keyboardType,
+    this.textInputAction,
+    this.obscureText = false,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextFormField(
+        controller: textCtrl,
+        decoration: new InputDecoration(
+          hintText: hint,
+          fillColor: Colors.grey[300].withOpacity(0.5),
+          prefixIcon: Icon(
+            icon,
+            color: CommonStyle.whiteColor,
+            size: 24,
+          ),
+          filled: true,
+          contentPadding: const EdgeInsets.only(top: 16, bottom: 16, left: 200),
+          isDense: true,
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.0,
+            ),
+          ),
+          errorStyle: TextStyle(fontSize: 14.0),
+          hintStyle: CommonStyle.defaultText(context, color: CommonStyle.whiteColor),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xfffafafa)),
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xfffafafa)),
+            borderRadius: BorderRadius.all(Radius.circular(12.0)),
+          ),
+        ),
+        textInputAction: textInputAction,
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        validator: validator,
+        onChanged: (value) => onChangeText(value),
+      ),
+    );
+  }
+}

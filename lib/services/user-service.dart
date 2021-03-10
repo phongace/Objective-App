ea5b58d22e96b54a3f702feeb39da324b70664af
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-10 17:16:57 +0700

    dsadsad

---
+++ lib/services/user-service.dart
@@ -0,0 +1,18 @@
+import 'package:objective/config/constant.dart';
+import 'package:objective/models/user/user.dart';
+import 'package:objective/plugin/dio.dart';
+import 'package:objective/response/response-data.dart';
+
+abstract class UserService {
+  static Future<User> getUser() async {
+    try {
+      final response = await http.get(Constant.apiUrl + 'user');
+      ResponseData responseData = response.data;
+      dynamic map = responseData.data;
+      User user = User.fromJson(map);
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+}
